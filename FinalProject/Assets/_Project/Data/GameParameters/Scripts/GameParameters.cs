3323c85b244d4c79640b2cc93f4674b7b14bdb8f
Brandon Lyman <66562222+blyman94@users.noreply.github.com>
2024-04-05 22:56:09 -0400

    Top-down movement start

---
+++ FinalProject/Assets/_Project/Data/GameParameters/Scripts/GameParameters.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+/// <summary>
+/// A VS-friendly data storage class for all parameters in the game. Keep a 
+/// reference to a GameParameters variable in the VisualScriptingSceneVariable
+/// list and use the getter methods to pull data from a designated parameter
+/// file.
+/// </summary>
+[CreateAssetMenu]
+public class GameParameters : ScriptableObject
+{
+    #region Data Fields
+    [Header("Player Movement")]
+
+    [SerializeField]
+    [Tooltip("What is the player's base initial jump velocity?")]
+    private float _playerJumpVelocityBase = 10.0f;
+
+    [SerializeField]
+    [Tooltip("How fast can the player move on land?")]
+    private float _playerMoveSpeedLand = 3.0f;
+
+    [SerializeField]
+    [Tooltip("How fast can the player move aboard a ship?")]
+    private float _playerMoveSpeedShip = 3.0f;
+    #endregion
+
+    #region Getters
+    public float PlayerJumpVelocityBase
+    {
+        get
+        {
+            return _playerJumpVelocityBase;
+        }
+    }
+    public float PlayerMoveSpeedLand
+    {
+        get
+        {
+            return _playerMoveSpeedShip;
+        }
+    }
+    public float PlayerMoveSpeedShip
+    {
+        get
+        {
+            return _playerMoveSpeedShip;
+        }
+    }
+    #endregion
+}
